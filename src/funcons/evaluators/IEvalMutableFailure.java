6f7f29a19211a8a0fa3fb4a9d354ced3cf41883f
pinoval <pinoval@gmail.com>
2015-03-04 12:32:01 +0100

    Added another eval interface.

---
+++ src/funcons/evaluators/IEvalMutableFailure.java
@@ -0,0 +1,9 @@
+package funcons.evaluators;
+
+import funcons.entities.Abstraction;
+import funcons.entities.Failure;
+import funcons.entities.Value;
+
+public interface IEvalMutableFailure extends Abstraction<Failure, Value> {
+
+}
