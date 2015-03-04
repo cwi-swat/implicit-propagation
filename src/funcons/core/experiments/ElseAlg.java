894279c05100951ff16b288f68cdec43f34e02f7
pinoval <pinoval@gmail.com>
2015-03-04 11:36:01 +0100

    Added mutating version of failure algebras (else/whenTrue).

--- src/funcons/core/experiments/ElseAlg.java
+++ src/funcons/core/experiments/ElseAlg.java
@@ -5,6 +5,7 @@ import funcons.entities.Failure;
 import funcons.entities.None;
 import funcons.entities.Value;
 import funcons.evaluators.IEvalFailure;
+import funcons.evaluators.IEvalMutableFailure;
 
 public interface ElseAlg<X> {
 	X else_(X x, X y);
@@ -23,6 +24,24 @@ class ConcreteElseAlg implements ElseAlg<IEvalFailure>{
 				return new Pair<Value,Failure>(y.eval(new None()).getKey(), new Failure(false));
 			}else{
 				return rx;
+			}
+		};
+	}
+	
+}
+
+
+class ConcreteElseWithMutationAlg implements ElseAlg<IEvalMutableFailure>{
+
+	@Override
+	public IEvalMutableFailure else_(IEvalMutableFailure x, IEvalMutableFailure y) {
+		return (f) -> {
+			Value v = x.eval(f);
+			if (f.getBool()){
+				f.setBool(false);
+				return y.eval(f);
+			}else{
+				return v;
 			}
 		};
 	}
